8d8a1c9684b1b29aac1c0e06cb46fbe6c9adeca4
Emily <emig2578@gmail.com>
2017-12-08 17:06:15 -0800

    pull in code from master

--- app/src/main/java/com/example/emigm/touchstone/TS_Text_Widget.java
+++ app/src/main/java/com/example/emigm/touchstone/TS_Text_Widget.java
@@ -1,5 +1,7 @@
 package com.example.emigm.touchstone;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.content.Context;
@@ -14,6 +16,31 @@ public class TS_Text_Widget implements TS_Widget {
     public TS_Text_Widget(String label, Context context) {
         // create data field
         data = new EditText(context);
+
+        final EditText log_text = (EditText)findViewById(R.id.logEntryText);
+        log_text.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int before, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int count, int after) {
+
+                // TODO: is there a better way to keep track of this (so we don't call length() every time the text changes)?
+                if (s.length() > 0) {
+
+                    // TODO: figure out how to do the color change automatically - could override this function, but there's probably a button parameter
+                    log_submit_button.setEnabled(true);
+                    log_submit_button.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                }
+                else {
+                    log_submit_button.setEnabled(false);
+                    log_submit_button.setTextColor(getResources().getColor(R.color.colorPrimary));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
     }
 
     // Packages widget data into TS xml scheme
